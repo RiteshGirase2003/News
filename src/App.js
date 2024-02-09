a0daddab538428954b8fe4e21b344633059da6b1
Ritesh <riteshgirasecode@gmail.com>
2024-02-09 20:22:20 +0530

    my second commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
+import React ,{useState,useEffect} from 'react';
+
 import './App.css';
+import Fetch from './Component/Fetch';
+import Search from './Component/Search';
 
 function App() {
+  const apiKEY = '2bb2cc83197443f8b7c3ce9a4c6d5639'
+  const [selectedOptions, setSelectedOptions] = useState([]);
+  const [url,setUrl] = useState('https://newsapi.org/v2/top-headlines/sources?apiKey='+apiKEY)
+
+  const handleSearchSelection = (updatedOptions) => {
+    setSelectedOptions(updatedOptions);
+  };
+
+  useEffect(()=>{
+    let array=''
+    if (selectedOptions.length > 0) {
+      selectedOptions.map((option,index)=>{
+        array+=option.search+'='+option.option+'&'
+      })
+    }
+    setUrl(`https://newsapi.org/v2/top-headlines/sources?${array}apiKey=${apiKEY}`)
+  },[selectedOptions]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='main'>
+      <h1 className='heading'> News App </h1>
+      <Search onSearchSelection={handleSearchSelection} />
+      {/* <Search/> */}
+      <Fetch url={url}/>
     </div>
   );
 }
